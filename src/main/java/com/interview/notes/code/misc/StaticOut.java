bc9c6aa55255a5b5206d53d6b2350582317cdbbb
sunil soni <suniltct@gmail.com>
2021-12-24 02:20:26 +0530

    StaticOut
    Signed-off-by: sunil soni <suniltct@gmail.com>

---
+++ src/main/java/com/interview/notes/code/misc/StaticOut.java
@@ -0,0 +1,23 @@
+package com.interview.notes.code.misc;
+
+class StaticOut {
+    static int x;
+    static int y;
+
+    void add(int a, int b) {
+        x = a + b;
+        y = x + b;
+    }
+}
+
+class StaticUse {
+    public static void main(String args[]) {
+        StaticOut obj1 = new StaticOut();
+        StaticOut obj2 = new StaticOut();
+        int a = 2;
+        obj1.add(a, a + 1);
+        obj2.add(5, a);
+        System.out.println(obj1.x + " " + obj2.y);
+    }
+}
+
--- src/main/java/com/interview/notes/code/misc/TryCatchFinallyTest.java
+++ src/main/java/com/interview/notes/code/misc/TryCatchFinallyTest.java
@@ -3,6 +3,10 @@ package com.interview.notes.code.misc;
 public class TryCatchFinallyTest {
     public static void main(String[] args) {
         System.out.println(test());//3
+
+        System.out.println(10+30+"aaa");//3
+        System.out.println("aa"+ 10+30);//3
+
     }
 
     private static int test() {
