90fcdaf22dd00f018f9f20119fe756a5e2219f35
sunilsoni <sunilsoni@live.in>
2023-06-09 14:00:50 +0530

    EmployeeCountByDept
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/test/test9/EmployeeCountByDept.java
@@ -0,0 +1,32 @@
+package com.interview.notes.code.test.test9;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class EmployeeCountByDept {
+    public static void main(String[] args) {
+        List<Employee1> employees = new ArrayList<>();
+        // Assuming employees list is populated with Employee objects
+
+        Map<Integer, Long> employeeCountByDept = employees.stream()
+                .collect(Collectors.groupingBy(Employee1::getDeptId, Collectors.counting()));
+
+        // Displaying the employee count by department
+        for (Map.Entry<Integer, Long> entry : employeeCountByDept.entrySet()) {
+            System.out.println("Department ID: " + entry.getKey() + ", Employee Count: " + entry.getValue());
+        }
+    }
+}
+
+class Employee1 {
+    private int id;
+    private String name;
+    private double salary;
+    private int deptId;
+
+    // Constructor, getters, and setters
+
+    public int getDeptId() {
+        return deptId;
+    }
+}
