a35fd1055e6347976c68facc198eb5e0840658b6
sunilsoni <sunilsoni@live.in>
2023-11-09 22:10:09 +0530

    RemoveDuplicatesFromArrayList
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/months/nov23/test3/Test3.java
@@ -0,0 +1,64 @@
+package com.interview.notes.code.months.nov23.test3;
+
+public class Test3 {
+    public static void main(String[] args) {
+
+    }
+
+    int[] solution(int[] memory, int[][] queries) {
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int queryType = queries[i][0];
+            int size = queries[i][1];
+
+            if (queryType == 0) { // alloc
+                result[i] = alloc(memory, size);
+            } else if (queryType == 1) { // erase
+                result[i] = erase(memory, size);
+            }
+        }
+
+        return result;
+    }
+
+    int alloc(int[] memory, int size) {
+        for (int i = 0; i <= memory.length - size; i++) {
+            boolean canAllocate = true;
+            for (int j = 0; j < size; j++) {
+                if (memory[i + j] == 1) {
+                    canAllocate = false;
+                    break;
+                }
+            }
+            if (canAllocate) {
+                for (int j = 0; j < size; j++) {
+                    memory[i + j] = 1;
+                }
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int erase(int[] memory, int position) {
+        if (position >= memory.length || memory[position] == 0) { // Invalid or not allocated
+            return -1;
+        }
+
+        // Find the start of the allocated block
+        int start = position;
+        while (start > 0 && memory[start - 1] == 1) {
+            start--;
+        }
+
+        // Erase the block
+        while (start < memory.length && memory[start] == 1) {
+            memory[start] = 0;
+            start++;
+        }
+
+        return position;
+    }
+
+}
---
+++ src/main/java/com/interview/notes/code/months/nov23/test4/RemoveDuplicatesFromArrayList.java
@@ -0,0 +1,41 @@
+package com.interview.notes.code.months.nov23.test4;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class RemoveDuplicatesFromArrayList {
+    public static void main(String[] args) {
+        // Create an ArrayList with duplicate elements
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(3);
+
+        // Remove duplicates
+        List<Integer> uniqueNumbers = removeDuplicates(numbers);
+
+        // Print the unique elements
+        System.out.println("Unique numbers: " + uniqueNumbers);
+    }
+
+    public static <T> List<T> removeDuplicates(List<T> list) {
+        // Create a HashSet to store unique elements
+        HashSet<T> set = new HashSet<>();
+
+        // Create a new ArrayList to store the unique elements in the original order
+        List<T> uniqueList = new ArrayList<>();
+
+        for (T item : list) {
+            // If the element is not in the HashSet, add it to both the HashSet and the new ArrayList
+            if (set.add(item)) {
+                uniqueList.add(item);
+            }
+        }
+
+        return uniqueList;
+    }
+}
