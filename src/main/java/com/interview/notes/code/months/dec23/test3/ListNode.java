4573364dc1b8416616b5998aab1f3209313fa0f5
sunilsoni <sunilsoni@live.in>
2023-12-14 00:26:21 +0530

    ReverseLinkedList
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/months/dec23/test3/ListNode.java
@@ -0,0 +1,10 @@
+package com.interview.notes.code.months.dec23.test3;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/main/java/com/interview/notes/code/months/dec23/test3/Test3.java
@@ -0,0 +1,7 @@
+package com.interview.notes.code.months.dec23.test3;
+
+public class Test3 {
+    public static void main(String[] args) {
+
+    }
+}
