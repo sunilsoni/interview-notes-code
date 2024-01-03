ddc9a2111be38829da56d17f70980fa8b01e1403
sunilsoni <sunilsoni@live.in>
2024-01-04 02:48:17 +0530

    ParallelApiCalls
    Signed-off-by: sunilsoni <sunilsoni@live.in>

--- src/main/java/com/interview/notes/code/months/dec23/test6/Test6.java
+++ src/main/java/com/interview/notes/code/months/dec23/test6/Test6.java
@@ -1,7 +1,18 @@
 package com.interview.notes.code.months.dec23.test6;
 
 public class Test6 {
-    public static void main(String[] args) {
 
+
+    public static void foo(StringBuffer sb) {
+        sb.append("foo");  // Appends "foo" to the original sb
+        sb = new StringBuffer();  // Creates a new StringBuffer, sb now refers to this new object
+        sb.append("wow");  // Appends "wow" to the new StringBuffer, not affecting sbOriginal
     }
+
+    public static void main(String[] args) {
+        StringBuffer sbOriginal = new StringBuffer("SB1");
+        foo(sbOriginal);  // Calls foo with sbOriginal
+        System.out.println(sbOriginal);  // Prints sbOriginal
+    }
+
 }
