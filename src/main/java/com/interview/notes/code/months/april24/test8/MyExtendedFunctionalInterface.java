87b632232d43480d45c530ed92194f5047485dfb
sunilsoni <sunilsoni@live.in>
2024-04-19 11:38:26 +0530

    ZigzagPatternSorted
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/months/april24/test8/MyExtendedFunctionalInterface.java
@@ -0,0 +1,6 @@
+package com.interview.notes.code.months.april24.test8;
+
+//@FunctionalInterface
+interface MyExtendedFunctionalInterface extends MyFunctionalInterface {
+    void myOtherMethod(); // This interface now has two abstract methods
+}
---
+++ src/main/java/com/interview/notes/code/months/april24/test8/MyFunctionalInterface.java
@@ -0,0 +1,6 @@
+package com.interview.notes.code.months.april24.test8;
+
+@FunctionalInterface
+interface MyFunctionalInterface {
+    void myMethod();
+}
