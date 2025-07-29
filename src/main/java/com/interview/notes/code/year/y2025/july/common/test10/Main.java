7d042c876560aefcd05c65c964785d33a8e41344
sunilsoni <sunilsoni@live.in>
2025-07-29 23:58:53 +0530

    EulerPiCalculator
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/year/y2025/july/common/test10/Main.java
@@ -0,0 +1,35 @@
+package com.interview.notes.code.year.y2025.july.common.test10;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        
+        Future<Long> cubeSumFuture = executor.submit(() -> calculateSum(arr, 3));
+        Future<Long> fourthPowerSumFuture = executor.submit(() -> calculateSum(arr, 4));
+        Future<Long> fifthPowerSumFuture = executor.submit(() -> calculateSum(arr, 5));
+
+        long cubeSum = cubeSumFuture.get();
+        long fourthPowerSum = fourthPowerSumFuture.get();
+        long fifthPowerSum = fifthPowerSumFuture.get();
+
+        executor.shutdown();
+
+        System.out.println(cubeSum + " " + fourthPowerSum + " " + fifthPowerSum);
+    }
+
+    private static long calculateSum(int[] arr, int power) {
+        return Arrays.stream(arr)
+                .mapToLong(n -> (long) Math.pow(n, power))
+                .sum();
+    }
+}
