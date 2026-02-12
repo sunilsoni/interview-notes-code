4fede10e66b31945e11ebe33ec40a88b326fc0bc
sunilsoni <sunilsoni@live.in>
2026-02-12 20:14:34 +0530

    code fixes
    Signed-off-by: sunilsoni <sunilsoni@live.in>

---
+++ src/main/java/com/interview/notes/code/year/y2026/feb/USTechSolutions/test1/Main.java
@@ -0,0 +1,24 @@
+package com.interview.notes.code.year.y2026.feb.USTechSolutions.test1;
+
+class Parent {
+    static { System.out.println("4. Static "); }
+    { System.out.println("5. Instance Initialization Block "); }
+}
+
+public class Main extends Parent {
+
+    static { System.out.println("1. Static "); }
+
+    { System.out.println("2. Instance Initialization Block"); }
+
+    public Main() {
+        System.out.println("3. Constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--- Starting main method ---");
+        Main obj1 = new Main();
+        Main obj2 = new Main();
+        System.out.println("--- Main method finished ---");
+    }
+}
---
+++ src/main/java/com/interview/notes/code/year/y2026/feb/USTechSolutions/test2/StreamTest.java
@@ -0,0 +1,20 @@
+package com.interview.notes.code.year.y2026.feb.USTechSolutions.test2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> result = numbers.stream()
+            .filter(n -> n % 2 != 0)          // 1. Filter odd numbers
+            .map(n -> (n * n) / 2)            // 2. Square and divide by 2
+            .sorted(Comparator.reverseOrder())// 3. Reverse order
+            .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
